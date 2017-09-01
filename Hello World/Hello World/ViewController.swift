4ec0bc5b10f74910d0d1b4755f9675f07aff65e8
Geoff Husser <gahyvb@gmail.com>
2017-08-31 19:21:29 -0500

    Implemented working buttons

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var hello: UIButton!
+    @IBOutlet weak var clear: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func helloClick(_ sender: Any) {
+        label.text = "Hello World!"
+    }
+    
+    @IBAction func Clear(_ sender: Any) {
+        label.text = ""
+    }
 }
 
